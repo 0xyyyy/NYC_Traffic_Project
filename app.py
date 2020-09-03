bd4226da31a3a471432a9cdf2e4368025eb7f5e1
Yuta Moxley <ymoxley@gmail.com>
2020-09-03 17:07:37 -0500

    created connection to sqlite database

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import pandas as pd
+
+#Create engine to connect to sqlite database
+engine = create_engine("sqlite:///db.sqlite")
+
+#Check that engine connection is working 
+print(pd.read_sql("SELECT * FROM trraffic", engine))
