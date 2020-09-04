cd0e6fadd7d2e41228071cc1330f97dc3d6a6593
Yuta Moxley <ymoxley@gmail.com>
2020-09-04 10:52:34 -0500

    added circle layer to leaflet map, wrote some comments about future direction

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,9 @@
-var mymap = L.map('mapid').setView([40.7128, 74.0060], 1);
+var mymap = L.map('mapid').setView([40.7128, -74.0060], 12);
 
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'; 
 
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const tiles = L.tileLayer(tileUrl, { attribution });
+// Create circle layers with radius and fill color depending on the amount of traffic that passes through that intersection.
+L.circle([40.72, -74.0070], {radius: 500}).addTo(mymap);
 tiles.addTo(mymap)
