d34ebc9e99da3fd80fec594876e874036baf7f49
Yuta Moxley <ymoxley@gmail.com>
2020-09-08 19:13:32 -0500

    fixed dataset

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([40.7128, -74.0060], 12);
+var mymap = L.map('mapid').setView([40.819896, -73.930559], 12);
 
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'; 
 
@@ -7,3 +7,6 @@ const tiles = L.tileLayer(tileUrl, { attribution });
 // Create circle layers with radius and fill color depending on the amount of traffic that passes through that intersection.
 L.circle([40.72, -74.0070], {radius: 500}).addTo(mymap);
 tiles.addTo(mymap)
+
+
+// function myMap(latitude, longitude) 
